f9bb0c02512f7d4361f4ed5ae905a5079256696a
elizzakai <71152176+elizzakai@users.noreply.github.com>
2023-12-28 21:00:12 -0500

    updates

--- components/skills.tsx
+++ components/skills.tsx
@@ -47,9 +47,11 @@ export default function Skills() {
           </motion.li>
         ))}
       </ul>
+      hey
     </section>
   );
 }
+
 
 
 // "use client";
@@ -101,6 +103,7 @@ export default function Skills() {
 //           </motion.li>
 //         ))}
 //       </ul>
+//       hiii
 //     </section>
 //   );
 // }
@@ -140,7 +143,7 @@ export default function Skills() {
 // //       <SectionHeading>My skills</SectionHeading>
 // //       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
 // //         {skillsData.map((skill, index) => (
-// //           <li
+// //           <motion.li
 // //             className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
 // //             key={index}
 // //             variants={fadeInAnimationVariants}
@@ -152,9 +155,63 @@ export default function Skills() {
 // //             custom={index}
 // //           >
 // //             {skill}
-// //           </li>
+// //           </motion.li>
 // //         ))}
 // //       </ul>
 // //     </section>
 // //   );
 // // }
+
+
+// // // "use client";
+
+// // // import React from "react";
+// // // import SectionHeading from "./section-heading";
+// // // import { skillsData } from "@/lib/data";
+// // // import { useSectionInView } from "@/lib/hooks";
+// // // import { motion } from "framer-motion";
+
+// // // const fadeInAnimationVariants = {
+// // //   initial: {
+// // //     opacity: 0,
+// // //     y: 100,
+// // //   },
+// // //   animate: (index: number) => ({
+// // //     opacity: 1,
+// // //     y: 0,
+// // //     transition: {
+// // //       delay: 0.05 * index,
+// // //     },
+// // //   }),
+// // // };
+
+// // // export default function Skills() {
+// // //   const { ref } = useSectionInView("Skills");
+
+// // //   return (
+// // //     <section
+// // //       id="skills"
+// // //       ref={ref}
+// // //       className="mb-28 max-w-[53rem] scroll-mt-28 text-center sm:mb-40"
+// // //     >
+// // //       <SectionHeading>My skills</SectionHeading>
+// // //       <ul className="flex flex-wrap justify-center gap-2 text-lg text-gray-800">
+// // //         {skillsData.map((skill, index) => (
+// // //           <li
+// // //             className="bg-white borderBlack rounded-xl px-5 py-3 dark:bg-white/10 dark:text-white/80"
+// // //             key={index}
+// // //             variants={fadeInAnimationVariants}
+// // //             initial="initial"
+// // //             whileInView="animate"
+// // //             viewport={{
+// // //               once: true,
+// // //             }}
+// // //             custom={index}
+// // //           >
+// // //             {skill}
+// // //           </li>
+// // //         ))}
+// // //       </ul>
+// // //     </section>
+// // //   );
+// // // }
