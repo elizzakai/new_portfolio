1847962f3b28de28cdef4db45a5c4e3b84d44732
elizzakai <71152176+elizzakai@users.noreply.github.com>
2023-12-29 22:07:09 -0500

    contact

--- components/experience.tsx
+++ components/experience.tsx
@@ -14,7 +14,7 @@ import { useSectionInView } from "@/lib/hooks";
 // import { useTheme } from "@/context/theme-context";
 
 export default function Experience() {
-  // const { ref } = useSectionInView("Experience");
+  const { ref } = useSectionInView("Experience", .2);
 
   // const { ref } = useSectionInView("Experience");
 
@@ -44,7 +44,7 @@ export default function Experience() {
   // // const { theme } = useTheme();
 
   return (
-    <section id="experience" className="scroll-mt-28 mb-28 sm:mb-40">
+    <section id="experience" ref = {ref} className="scroll-mt-28 mb-28 sm:mb-40">
       <SectionHeading>My experience</SectionHeading>
       <VerticalTimeline lineColor="">
         {experiencesData.map((item, index) => (
